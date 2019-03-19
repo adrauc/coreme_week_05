535899ec9b8aff6ffd5a0bff0f25f328e31bd2b5
Adrian Rauchfleisch <adrian.rauchfleisch@gmail.com>
2019-03-19 12:21:13 +0800

    first commit

---
+++ week_05/initLesson.R
@@ -0,0 +1,33 @@
+# Code placed in this file fill be executed every time the
+      # lesson is started. Any variables created here will show up in
+      # the user's working directory and thus be accessible to them
+      # throughout the lesson.
+
+.get_course_path <- function(){
+  tryCatch(swirl:::swirl_courses_dir(),
+           error = function(c) {file.path(find.package("swirl"),"Courses")}
+  )
+}
+
+load(file.path(.get_course_path(), "coreme_week_05", "week_05", "tweets.rdata"))
+
+if(!require(tibble)){
+  install.packages("tibble")
+  library(tibble)
+}
+
+
+# Path to data
+
+# Read in data
+plants <- read.csv(file.path(.get_course_path(), "coreme_week_05", "week_05", "plant-data.txt"), strip.white=TRUE, na.strings="")
+
+# Remove annoying columns
+.cols2rm <- c('Accepted.Symbol', 'Synonym.Symbol')
+plants <- plants[, !(names(plants) %in% .cols2rm)]
+
+# Make names pretty
+names(plants) <- c('Scientific_Name', 'Duration', 'Active_Growth_Period',
+                   'Foliage_Color', 'pH_Min', 'pH_Max',
+                   'Precip_Min', 'Precip_Max',
+                   'Shade_Tolerance', 'Temp_Min_F')
